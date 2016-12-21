df8474a7f82e0a6f7ae9ec448fb83357b7e252e0
Kevin Hooke <kevin.hooke@gmail.com>
2016-12-20 22:18:47 -0800

    updated change handlers

--- react-es5/src/Components/CalculatorComponent.js
+++ react-es5/src/Components/CalculatorComponent.js
@@ -6,33 +6,38 @@ var Calculator = React.createClass({
     getInitialState : function(){
         return({
             value1: this.props.value1,
-            value2: this.props.value2
+            value2: this.props.value2,
+            result: 0
         });
     },
 
-    handleChange: function(e){
-        this.setState({
-            value1: e.target.value
-        })
+    handleChangeValue1: function(e){
+        this.setState({ value1 : e.target.value });
     },
-
+    handleChangeValue2: function(e){
+        this.setState({ value2 : e.target.value });
+    },
+    calculate : function(e){
+        e.preventDefault();
+        this.setState({ result : Number.parseInt(this.state.value1) + Number.parseInt(this.state.value2) });
+    },
     render: function() {
         return (
             <form>
                 <div>
                     <label>Value 1</label>
-                    <input id="value1" type="text" onChange={this.handleChange} value={this.state.value1}/>
+                    <input name="value1" type="text" onChange={this.handleChangeValue1} value={this.state.value1}/>
                 </div>
                 <div>
                     <label>Value 2</label>
-                    <input id="value2" type="text" value={this.props.value2}/>
+                    <input name="value2" type="text" onChange={this.handleChangeValue2} value={this.state.value2}/>
                 </div>
                 <div>
                     <label>Result:</label>
-                    <div id="result"></div>
+                    <div id="result">{this.state.result}</div>
                 </div>
                 <div>
-                    <button>Calculate</button>
+                    <button onClick={this.calculate}>Calculate</button>
                 </div>
 
             </form>
